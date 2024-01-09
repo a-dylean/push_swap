efc18c393ec98a620517f71dcbe305d2210b09b6
Alisa Tonkopiy <atonkopi@bess-f4r4s5.clusters.42paris.fr>
2024-01-09 19:52:50 +0100

    moved sorting files to one file

--- src/sorting.c
+++ src/sorting.c
@@ -1,16 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_complex.c                                     :+:      :+:    :+:   */
+/*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 15:00:59 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/01/09 17:55:32 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/01/09 19:26:32 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	ft_sort_two(t_stack **stack_a)
+{
+	if ((*stack_a)->num > (*stack_a)->next->num)
+		ft_swap(stack_a, 'a');
+}
+
+void	ft_sort_three(t_stack **stack_a)
+{
+	int	first;
+	int	second;
+	int	third;
+
+	first = (*stack_a)->num;
+	second = (*stack_a)->next->num;
+	third = (*stack_a)->next->next->num;
+	if (first > second && second < third && first < third)
+		ft_swap(&*stack_a, 'a');
+	else if (first > second && second > third && first > third)
+	{
+		ft_swap(&*stack_a, 'a');
+		ft_reverse_rotate(&*stack_a, 'a');
+	}
+	else if (first > second && second < third && first > third)
+		ft_rotate(&*stack_a, 'a');
+	else if (first < second && second > third && first < third)
+	{
+		ft_swap(&*stack_a, 'a');
+		ft_rotate(&*stack_a, 'a');
+	}
+	else if (first < second && second > third && first > third)
+		ft_reverse_rotate(&*stack_a, 'a');
+}
 
 void	ft_sort_complex(t_stack **stack_a, t_stack **stack_b)
 {
