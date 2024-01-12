384b2f78f4369e569f117aeed91474a422e25046
Alisa Tonkopiy <atonkopi@bess-f4r4s10.clusters.42paris.fr>
2024-01-12 17:48:51 +0100

    added helper funcs file

--- src/errors_check.c
+++ src/errors_check.c
@@ -6,38 +6,18 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:21:15 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/01/12 17:39:24 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/01/12 17:48:23 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	ft_chars_check(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str[i])
-		ft_exit();
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]) && str[i] != ' ' && str[i] != '-'
-			&& str[i] != '+')
-		{
-			ft_exit();
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
 char	**ft_argv_check(char **argv)
 {
 	int		i;
+	int		j;
 	char	**temp;
 	char	**temp2;
-	int		j;
 
 	temp2 = malloc(2);
 	if (!temp2)
@@ -59,6 +39,25 @@ char	**ft_argv_check(char **argv)
 	}
 	argv = temp2;
 	return (argv);
+}
+int	ft_chars_check(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str[i])
+		ft_exit();
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]) && str[i] != ' ' && str[i] != '-'
+			&& str[i] != '+')
+		{
+			ft_exit();
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
 
 void	ft_intsize_check(char **argv)
@@ -93,39 +92,4 @@ void	ft_duplicates_check(t_node *stack)
 		}
 		temp = temp->next;
 	}
-}
-
-char	**ft_add_string_to_array(char **array, char *new_str)
-{
-	int		i;
-	int		j;
-	char	**new_array;
-
-	i = 0;
-	j = 0;
-	while (array[i])
-		i++;
-	new_array = malloc((i + 2) * sizeof(char *));
-	if (!new_array)
-		return (NULL);
-	while (j < i)
-	{
-		new_array[j] = array[j];
-		j++;
-	}
-	new_array[i] = ft_strdup(new_str);
-	if (!new_array[i])
-	{
-		free(new_array);
-		return (NULL);
-	}
-	new_array[i + 1] = NULL;
-	free(array);
-	return (new_array);
-}
-
-void	ft_exit(void)
-{
-	ft_putstr_fd("Error\n", 2);
-	exit(0);
 }
---
+++ src/helper_funcs.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helper_funcs.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 17:46:24 by atonkopi          #+#    #+#             */
+/*   Updated: 2024/01/12 17:46:39 by atonkopi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+char	**ft_add_string_to_array(char **array, char *new_str)
+{
+	int		i;
+	int		j;
+	char	**new_array;
+
+	i = 0;
+	j = 0;
+	while (array[i])
+		i++;
+	new_array = malloc((i + 2) * sizeof(char *));
+	if (!new_array)
+		return (NULL);
+	while (j < i)
+	{
+		new_array[j] = array[j];
+		j++;
+	}
+	new_array[i] = ft_strdup(new_str);
+	if (!new_array[i])
+	{
+		free(new_array);
+		return (NULL);
+	}
+	new_array[i + 1] = NULL;
+	free(array);
+	return (new_array);
+}
+
+void	ft_exit(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit(0);
+}
