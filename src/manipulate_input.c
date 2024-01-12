cc5020c2cab5ac9cd5d15f8e602cefd42590bd05
Alisa Tonkopiy <atonkopi@bess-f4r4s10.clusters.42paris.fr>
2024-01-12 19:11:58 +0100

    small change

--- src/manipulate_input.c
+++ src/manipulate_input.c
@@ -6,7 +6,7 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:04:12 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/01/12 16:58:36 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/01/12 17:53:30 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ void	ft_create_list(t_node **stack, char **argv)
 	int		i;
 
 	i = 0;
-	// if (argc == 2)
-	// 	i = 0;
 	while (argv[i])
 	{
 		next = ft_create_node(ft_atoi(argv[i]), i);
